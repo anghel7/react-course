77f874d043dff175c24b18e1783c9fcf220d8713
Angel Martin Antezana Flores <ghehahanghel@gmail.com>
2024-09-10 22:43:55 -0400

    feat: Adding project infrastructure and add example card

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from './components/mode-toggle'
+import Home from './pages/home/Home'
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <ModeToggle></ModeToggle>
+      <Home></Home>
     </ThemeProvider>
   )
 }
