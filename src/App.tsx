99208c5fb19d3cf7aa79cdab0203b347a8e12a23
Angel Martin Antezana Flores <ghehahanghel@gmail.com>
2024-09-17 22:37:12 -0400

    Retrieve a list of pokemons using axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import './App.css'
 
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from './components/mode-toggle'
+import PokeList from './course-examples/poke-list'
 // import Home from './pages/home/Home'
-import ControlledForm from './course-examples/controlled-form'
-import NoControlledForm from './course-examples/no-controlled-form'
+// import ControlledForm from './course-examples/controlled-form'
+// import NoControlledForm from './course-examples/no-controlled-form'
 
 export function App() {
   return (
@@ -12,7 +13,8 @@ export function App() {
       <ModeToggle></ModeToggle>
       {/* <Home></Home> */}
       {/* <ControlledForm></ControlledForm> */}
-      <NoControlledForm></NoControlledForm>
+      {/* <NoControlledForm></NoControlledForm> */}
+      <PokeList></PokeList>
     </ThemeProvider>
   )
 }
---
+++ src/course-examples/poke-list.tsx
@@ -0,0 +1,83 @@
+import { FC, useEffect, useState } from 'react';
+import { api } from './api';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { AxiosResponse } from 'axios';
+
+
+type PokeData = {
+  count: number,
+  next: string,
+  previous: string,
+  results: PokeItemListDto[]
+}
+
+interface PokeItemListDto {
+  name: string,
+  url: string
+}
+
+const PokeList: FC = () => {
+  const [list, setList] = useState<PokeItemListDto[]>([]);
+  const [count, setCount] = useState<number>(0);
+  useEffect(() => {
+    const getPoke = async () => {
+      const response = await getPokeList();
+      if (response.status == 200) {
+        setList(response.data.results)
+        setCount(response.data.count)
+      }
+    }
+    getPoke();
+  }, []);
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: 'center' }}>
+        <h1>Poke List</h1>
+      </div>
+      <Table>
+        <TableCaption>A list of pokes.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Poke Name</TableHead>
+            <TableHead className="text-right">Poke Url</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {list.map((poke, index) => (
+            <TableRow key={index}>
+              <TableCell className="font-medium">{poke.name}</TableCell>
+              <TableCell className="text-right"><a href="#" className='text-sky-600'>{poke.url}</a></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">{count}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
+  )
+}
+
+const getPokeList = async () => {
+  try {
+    const response: AxiosResponse<PokeData, any> = await api.get("pokemon");
+    return response;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
+
+export default PokeList
