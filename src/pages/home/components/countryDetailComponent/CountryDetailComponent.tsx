2411e4e010e7a76673599de95d76f167e3d4c602
Angel Martin Antezana Flores <ghehahanghel@gmail.com>
2024-09-27 19:36:03 -0400

    feat: implement country detail page

--- src/pages/home/components/countryDetailComponent/CountryDetailComponent.tsx
+++ src/pages/home/components/countryDetailComponent/CountryDetailComponent.tsx
@@ -17,6 +17,7 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
+import { Badge } from "@/components/ui/badge"
 
 
 import './CountryDetailComponent.css'
@@ -25,7 +26,42 @@ const CountryDetailComponent: FC = () => {
   const [country, setCountry] = useState<Country>(COUNTRY);
   return (
     <>
-      <h1>Country Detail Component Works!!!</h1>
+      <div className='main-container'>
+        <div className='detail-container'>
+          <div className='flag-container'>
+            <img className="profile-photo" src={country.flags.png} alt={"Carlie Anglemire"} />
+          </div>
+          <div className='info-container'>
+            <p className='font-semibold text-2xl'>{country.name}</p>
+            <div className='info-grid'>
+              <div className='info-grid-sub-container'>
+                <p><span className='font-semibold'>Native Name: </span>{country.nativeName}</p>
+                <p><span className='font-semibold'>Population: </span>{country.population}</p>
+                <p><span className='font-semibold'>Region: </span>{country.region}</p>
+                <p><span className='font-semibold'>Sub Region: </span>{country.subregion}</p>
+                <p><span className='font-semibold'>Capital: </span>{country.capital}</p>
+              </div>
+              <div className='info-grid-sub-container'>
+                <p><span className='font-semibold'>Top Level Domain: </span>{country.topLevelDomain?.map((domain: string, index: number) => (
+                  <span key={index}>{domain}</span>
+                ))}</p>
+                <p><span className='font-semibold'>Currencies: </span>{country.currencies?.map((currency: Currency, index: number) => (
+                  <span key={index}>{currency.name}</span>
+                ))}</p>
+                <p><span className='font-semibold'>Languages: </span>{country.languages.map((lan: Language, index: number) => (
+                  <span key={index}>{lan.name}</span>
+                ))}</p>
+              </div>
+            </div>
+            <div className='border-container'>
+              <p>Border Countries:</p>
+              {country.borders?.map((borderCountry: string, index: number) => (
+                <Badge variant="outline" key={index}>{borderCountry}</Badge>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
