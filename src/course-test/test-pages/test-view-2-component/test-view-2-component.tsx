e2d209b81f64492e1be03a8f135f52f0c20fb2bf
Angel Martin Antezana Flores <ghehahanghel@gmail.com>
2024-09-28 20:05:54 -0400

    feat: adding axios request

--- src/course-test/test-pages/test-view-2-component/test-view-2-component.tsx
+++ src/course-test/test-pages/test-view-2-component/test-view-2-component.tsx
@@ -1,9 +1,21 @@
 import TestButtonComponent from '@/course-test/test-components/test-buton-component/test-button-component';
 import TestCardComponent from '@/course-test/test-components/test-card-component/test-card-component';
 import TestNavigationBarComponent from '@/course-test/test-components/test-navigation-bar-component/test-navigation-bar-component';
-import { FC, } from 'react';
+import { FC, useEffect, useState, } from 'react';
+import { AxiosResponse } from 'axios';
 
 const TestView2Component: FC = (props) => {
+  const [list, setList] = useState<PokeItemListDto[]>([]);
+  useEffect(() => {
+    const getPoke = async () => {
+      const response = await getPokeList();
+      if (response.status == 200) {
+        console.log('Poke list: ', response.data.results);
+        setList(response.data.results)
+      }
+    }
+    getPoke();
+  }, []);
 
   return (
     <>
@@ -16,3 +28,33 @@ const TestView2Component: FC = (props) => {
 }
 
 export default TestView2Component
+
+type PokeData = {
+  count: number,
+  next: string,
+  previous: string,
+  results: PokeItemListDto[]
+}
+
+interface PokeItemListDto {
+  name: string,
+  url: string
+}
+
+const getPokeList = async () => {
+  try {
+    const response: AxiosResponse<PokeData, any> = await pokeApi.get("pokemon");
+    return response;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
+
+import axios from "axios";
+export const pokeApi = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/',
+    timeout: 1000,
+    headers:{
+        "Content-Type": "application/json"
+    }
+});
